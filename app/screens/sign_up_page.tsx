7e0eabc67e0bc61a4702a7c5e4ef5629eded4e73
jestoniandales25 <andalesjestoni1998@gmail.com>
2024-09-17 16:10:27 +0800

    Updated Sign Up Page

--- app/screens/sign_up_page.tsx
+++ app/screens/sign_up_page.tsx
@@ -1,11 +1,34 @@
-import { Text, View } from "react-native";
+import React, { useState } from 'react';
+import { Text, View, TextInput, TouchableOpacity } from 'react-native';
+import signupStyles from "../../styles/sign_up_styles";
 
-export default function sign_up_page() {
-    return(
-        <View>
-            <Text>
-                Tits
-            </Text>
-        </View>
-    );
-}
+
+const signup = ({ onGoBack }) => { 
+  return (
+    <View style={signupStyles.container}>
+      <Text style={signupStyles.textHeader}>Sign Up</Text>
+      <TextInput 
+        style={signupStyles.signupInput}
+        placeholder='Username'
+      />
+      <TextInput 
+        style={signupStyles.signupInput}
+        placeholder='Email'
+      />
+      <TextInput 
+        style={signupStyles.signupInput}
+        placeholder='Password'
+      />
+      <TextInput 
+        style={signupStyles.signupInput}
+        placeholder='Corfirm Password'
+      />
+      <TouchableOpacity style={signupStyles.button} onPress={onGoBack}>
+        <Text style={signupStyles.text}>Login</Text>
+      </TouchableOpacity>
+      <Text style={signupStyles.textOut}>Already have an account?</Text>
+    </View>
+  );
+};
+
+export default signup
