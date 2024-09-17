96fdb3768a40bedde899be0f9ed3449b14fd76b5
jestoniandales25 <andalesjestoni1998@gmail.com>
2024-09-17 19:34:23 +0800

    hide and show password and confirm password

--- app/screens/sign_up_page.tsx
+++ app/screens/sign_up_page.tsx
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
 import { Text, View, TextInput, TouchableOpacity } from 'react-native';
 import signupStyles from "../../styles/sign_up_styles";
+import { Ionicons } from '@expo/vector-icons'; // Import Ionicons
 
 
 const signup = ({ onGoBack }) => { 
+
+  const [showPassword, setShowPassword] = useState(false); // State for password visibility
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false); // State for confirm password visibility
+
   return (
     <View style={signupStyles.container}>
       <Text style={signupStyles.textHeader}>Sign Up</Text>
@@ -15,18 +20,46 @@ const signup = ({ onGoBack }) => {
         style={signupStyles.signupInput}
         placeholder='Email'
       />
-      <TextInput 
-        style={signupStyles.signupInput}
-        placeholder='Password'
-      />
-      <TextInput 
-        style={signupStyles.signupInput}
-        placeholder='Corfirm Password'
-      />
-      <TouchableOpacity style={signupStyles.button} onPress={onGoBack}>
+      <View style={{ flexDirection: 'row', alignItems: 'center' }}> 
+        <TextInput 
+          style={[signupStyles.signupInput]} 
+          placeholder='Password'
+          secureTextEntry={!showPassword} // Toggle password visibility
+        />
+        <TouchableOpacity  style={{ position: 'absolute', right: 30 }}
+          onPress={() => setShowPassword(!showPassword)}>
+          <Ionicons 
+            name={showPassword ?  'eye-outline' : 'eye-off-outline'} // Toggle icon
+            size={24} 
+            color="black" 
+          />
+        </TouchableOpacity>
+      </View>
+      
+      <View style={{ flexDirection: 'row', alignItems: 'center' }}> 
+        <TextInput 
+          style={[signupStyles.signupInput]} 
+          placeholder='Confirm Password'
+          secureTextEntry={!showConfirmPassword} // Toggle password visibility
+        />
+        <TouchableOpacity  style={{ position: 'absolute', right: 30 }}
+          onPress={() => setShowConfirmPassword(!showConfirmPassword)}>
+          <Ionicons 
+            name={showConfirmPassword ?  'eye-outline' : 'eye-off-outline'} // Toggle icon
+            size={24} 
+            color="black" 
+          />
+        </TouchableOpacity>
+      </View>
+
+      <TouchableOpacity style={signupStyles.button}>
         <Text style={signupStyles.text}>Login</Text>
       </TouchableOpacity>
-      <Text style={signupStyles.textOut}>Already have an account?</Text>
+      <Text style={signupStyles.textOut}>Already have an account?
+        <TouchableOpacity onPress={onGoBack}>
+          <Text style={signupStyles.textCallLogin}>Login</Text>
+        </TouchableOpacity>
+      </Text>
     </View>
   );
 };
