bba065418117ae39055f6d31ed5ce06bc645e945
jestoniandales25 <andalesjestoni1998@gmail.com>
2024-09-18 08:57:49 +0800

    Change the render to navigation

--- app/screens/log_in_page.tsx
+++ app/screens/log_in_page.tsx
@@ -2,18 +2,13 @@ import React, { useState } from "react";
 import { Text, View, TextInput, TouchableOpacity } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import AppStyles from "../../styles/log_in_styles";
-import Signup from "../screens/sign_up_page";
 import { useNavigation } from "@react-navigation/native";
 import { StackActions } from '@react-navigation/native';
-
-
 
 export default function App() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [showPassword, setShowPassword] = useState(false); // State for password visibility
-  const [isSignUpVisible, setIsSignUpVisible] = useState(false);
-
   const navigation = useNavigation();
 
   const handleLogin = () => {
@@ -30,51 +25,43 @@ export default function App() {
   };
 
   const handleSignup = () => {
-    setIsSignUpVisible(true); // Show the Signup component
-  };
-
+    // Navigate to the Sign Up screen
+    navigation.dispatch(
+        StackActions.replace('Sign Up')
+    );
+};
 
   return (
     <View style={AppStyles.container}>
-
-      {isSignUpVisible ? ( // Conditionally render Signup
-        <Signup onGoBack={() => setIsSignUpVisible(false)} />
-      ) : (
-        <>
-          <TextInput
-            style={AppStyles.userInput}
-            placeholder='Username'
-            onChangeText={(val) => setUsername(val)}
+      <TextInput
+        style={AppStyles.userInput}
+        placeholder='Username'
+        onChangeText={(val) => setUsername(val)}
+      />
+      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+        <TextInput
+          style={[AppStyles.userInput]} // Take up available space
+          placeholder='Password'
+          onChangeText={(val) => setPassword(val)}
+          secureTextEntry={!showPassword} // Toggle password visibility
+        />
+        <TouchableOpacity style={{ position: 'absolute', right: 30 }}
+          onPress={() => setShowPassword(!showPassword)}>
+          <Ionicons
+            name={showPassword ? 'eye-outline' : 'eye-off-outline'} // Toggle icon
+            size={24}
+            color="black"
           />
-
-          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-            <TextInput
-              style={[AppStyles.userInput]} // Take up available space
-              placeholder='Password'
-              onChangeText={(val) => setPassword(val)}
-              secureTextEntry={!showPassword} // Toggle password visibility
-            />
-            <TouchableOpacity style={{ position: 'absolute', right: 30 }}
-              onPress={() => setShowPassword(!showPassword)}>
-              <Ionicons
-                name={showPassword ? 'eye-outline' : 'eye-off-outline'} // Toggle icon
-                size={24}
-                color="black"
-              />
-            </TouchableOpacity>
-          </View>
-
-          <TouchableOpacity style={AppStyles.button} onPress={handleLogin}>
-            <Text style={AppStyles.text}>Login</Text>
-          </TouchableOpacity>
-          <Text style={AppStyles.textOut}>Don't have an account?
-            <TouchableOpacity onPress={handleSignup}>
-              <Text style={AppStyles.textCallSignUp}>Sign Up</Text>
-            </TouchableOpacity>
-          </Text>
-
-        </>
-      )}
+        </TouchableOpacity>
+      </View>
+      <TouchableOpacity style={AppStyles.button} onPress={handleLogin}>
+        <Text style={AppStyles.text}>Login</Text>
+      </TouchableOpacity>
+      <Text style={AppStyles.textOut}>Don't have an account?
+        <TouchableOpacity onPress={handleSignup}>
+          <Text style={AppStyles.textCallSignUp}>Sign Up</Text>
+        </TouchableOpacity>
+      </Text>
     </View>
   );
 
